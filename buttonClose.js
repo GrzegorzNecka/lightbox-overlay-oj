a43f4c8da1101b0ea2aaf3772821d1cc5a65145c
GrzegorzNecka <strzypior@gmail.com>
2021-06-24 21:47:29 +0200

    1 file modified

--- buttonClose.js
+++ buttonClose.js
@@ -3,9 +3,10 @@ import Vue from 'vue';
 const buttonClose = () =>
   Vue.component('button-close', {
     template: `
-        <div class="absolute right-0 top-0 -mr-2 -mt-2 border cursor-pointer shadow-lg bg-white z-10 p-1 rounded-full p-2">    
-            <img src="https://image.flaticon.com/icons/svg/151/151882.svg"/ class="w-2 h-2">
-        </div>
+    <div class="absolute right-0 top-0 -mr-2 -mt-2 border cursor-pointer shadow-lg bg-white z-10 p-1 rounded-full p-2">
+        <img src="https://image.flaticon.com/icons/svg/151/151882.svg" class="w-2 h-2">
+    </div>
+    
       `
   });
 
